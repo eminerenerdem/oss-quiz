2978fe528e21bfc84bf33abf8df59f98a6b30759
Emin Eren Erdem <erenerdem1999@gamil.com>
2021-12-28 10:51:36 +0300

    First answer

---
+++ answer1.sh
@@ -0,0 +1,43 @@
+# Q1
+
+echo -num "Enter numnber : "
+read num
+ 
+rem=$(( $num % 2 ))
+ 
+if [ $rem -eq 0 ]
+then    
+  echo "$num is even number"
+else
+  for (( i=1;i<=$num ;i++))
+do
+   for (( j=$num;j>=i;j-- ))
+   do
+   echo -n " "
+   done
+   for (( c=1;c<=i;c++ ))
+   do
+   echo -n " *"
+   sum=`expr $sum + 1`
+   done
+echo ""
+done
+d_max=`expr $num - 1`
+for (( i=$d_max;i>=1;i--))
+do
+   for (( j=i;j<=$d_max;j++ ))
+   do
+   if [ $j -eq $d_max ]
+   then
+   echo -n " "
+   fi
+   echo -n " "
+   done
+   for (( c=1;c<=i;c++ ))
+   do
+   echo -n " *"
+   sum=`expr $sum + 1`
+   done
+echo ""
+done
+fi
